359283fc1a834dfdf33c22d8d4b15882f217bde7
Avunu LLC <mail@avu.nu>
2024-09-16 23:09:22 -0400

    fixes

--- flake.nix
+++ flake.nix
@@ -5,167 +5,198 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-    lib = nixpkgs.lib;
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      lib = nixpkgs.lib;
 
-    # Common configuration module
-    commonModule = { config, pkgs, lib, ... }: {
+      # Common configuration module
+      commonModule =
+        { config, pkgs, ... }:
+        {
+          boot = {
+            kernel.sysctl = {
+              "vm.nr_hugepages" = 1024;
+            };
+            kernelModules = [
+              "vfio_pci"
+              "uio_pci_generic"
+              "nvme-tcp"
+            ];
+            kernelPackages = pkgs.linuxPackages_latest;
+            loader.grub.device = "/dev/sda";
+          };
 
-      boot = {
-        kernel.sysctl = {
-          "vm.nr_hugepages" = 1024;
-        };
-        kernelModules = [
-          "vfio_pci"
-          "uio_pci_generic"
-          "nvme-tcp"
-        ];
-        kernelPackages = pkgs.linuxPackages_latest;
-        loader.grub.device = "/dev/sda";
-      };
+          documentation = {
+            enable = false;
+            doc.enable = false;
+            man.enable = false;
+            nixos.enable = false;
+          };
 
-      documentation = {
-        enable = mkDefault false;
-        doc.enable = mkDefault false;
-        man.enable = mkDefault false;
-        nixos.enable = mkDefault false;
-      };
+          imports = [
+            "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+          ];
 
-      imports = [
-        "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
-      ];
+          networking = {
+            dhcpcd.enable = false;
+            interfaces.eth0.useDHCP = false;
+            useNetworkd = true;
+          };
 
-      networking = {
-        defaultGateway = { address = "10.24.0.1"; interface = "eth0"; };
-        dhcpcd.enable = false;
-        interfaces.eth0.useDHCP = false;
-      };
+          nix = {
+            gc = {
+              automatic = true;
+              dates = "weekly";
+              options = "--delete-older-than 7d";
+            };
+            settings.experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+          };
 
-      nix = {
-        gc = {
-          automatic = mkDefault true;
-          dates = mkDefault "weekly";
-          options = mkDefault "--delete-older-than 7d";
-        };
-        settings.experimental-features = mkDefault [ "nix-command" "flakes" ];
-      };
+          fileSystems."/" = {
+            device = "/dev/sda1";
+            fsType = "ext4";
+          };
 
-      fileSystems."/" = {
-        device = "/dev/sda1";
-        fsType = "ext4";
-      };
+          services = {
+            cloud-init = {
+              enable = true;
+              network.enable = true;
+              config = ''
+                system_info:
+                  distro: nixos
+                  network:
+                    renderers: [ 'networkd' ]
+                  default_user:
+                    name: ops
+                users:
+                    - default
+                ssh_pwauth: false
+                chpasswd:
+                  expire: false
+                cloud_init_modules:
+                  - migrator
+                  - seed_random
+                  - growpart
+                  - resizefs
+                cloud_config_modules:
+                  - disk_setup
+                  - mounts
+                  - set-passwords
+                  - ssh
+                cloud_final_modules: []
+              '';
+            };
 
-      services = {
-        cloud-init = {
-          enable = true;
-          network.enable = true;
-          config = ''
-            system_info:
-              distro: nixos
-              network:
-                renderers: [ 'networkd' ]
-              default_user:
-                name: ops
-            users:
-                - default
-            ssh_pwauth: false
-            chpasswd:
-              expire: false
-            cloud_init_modules:
-              - migrator
-              - seed_random
-              - growpart
-              - resizefs
-            cloud_config_modules:
-              - disk_setup
-              - mounts
-              - set-passwords
-              - ssh
-            cloud_final_modules: []
-            '';
-        };
-        k3s = {
-          enable = true;
-          tokenFile = "/etc/k3s/tokenFile";
-          environmentFile = "/etc/k3s/envs";
-        };
-        fstrim = {
-          enable = mkDefault true;
-          interval = mkDefault "daily";
-        };
-        openssh.enable = true;
-        qemuGuest.enable = true;
-      };
-
-      users.users.ops = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" ];
-      };
-
-      systemd.network.enable = true;
-
-      security.sudo.wheelNeedsPassword = false;
-
-      system.autoUpgrade = {
-        enable = true;
-      };
-
-      zramSwap.enable = mkDefault true;
-
-    };
-  in {
-    packages.${system} = {
-      serverImage = pkgs.nixosConfigurations.server.config.system.build.diskImage;
-      agentImage = pkgs.nixosConfigurations.agent.config.system.build.diskImage;
-    };
-
-    nixosConfigurations = {
-      server = pkgs.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          commonModule
-          { config, pkgs, lib, ... }: {
-            # Server-specific configuration
-            services.k3s = {
+            k3s = {
+              enable = true;
               extraFlags = [
-                "--no-deploy traefik"
-                "--cluster-cidr 10.24.0.0/16"
+                "--disable traefik" # Disable default ingress as we'll use Octavia
+                "--disable servicelb" # Disable default load balancer
+                "--disable local-storage" # Disable default storage as we'll use Longhorn
               ];
-              role = "server";
-              disableAgent = true;
+              tokenFile = "/etc/k3s/tokenFile";
+              environmentFile = "/etc/k3s/envs";
             };
 
-            # Auto-upgrade settings
-            system.autoUpgrade = {
-              flake = "github:avunu/k3s#server";
-              allowReboot = false;
+            fstrim = {
+              enable = true;
+              interval = "daily";
             };
-          }
-        ];
+
+            openssh.enable = true;
+
+            qemuGuest.enable = true;
+          };
+
+          users.users.ops = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+          };
+
+          systemd.network.enable = true;
+
+          security.sudo.wheelNeedsPassword = false;
+
+          system = {
+            autoUpgrade = {
+              enable = true;
+              rebootWindow = {
+                lower = "01:00";
+                upper = "05:00";
+              };
+              randomizedDelaySec = "45min";
+            };
+            stateVersion = "24.05";
+          };
+
+          zramSwap.enable = true;
+        };
+    in
+    {
+      packages.${system} = {
+        serverImage = self.nixosConfigurations.server.config.system.build.openstackImage;
+        agentImage = self.nixosConfigurations.agent.config.system.build.openstackImage;
       };
 
-      agent = pkgs.nixosSystem {
-        system = "x86_64-linux";
+      nixosConfigurations = {
+
+        agent = lib.nixosSystem {
+          inherit system;
+          modules = [
+            commonModule
+            (
+              { config, pkgs, ... }:
+              {
+                # Agent-specific configuration
+                services.k3s = {
+                  serverAddr = "https://10.24.0.254:6443";
+                  role = "agent";
+                };
+
+                # Auto-upgrade settings
+                system.autoUpgrade = {
+                  flake = "github:avunu/k3s#agent";
+                  allowReboot = true;
+                };
+              }
+            )
+          ];
+        };
+      };
+
+      server = lib.nixosSystem {
+        inherit system;
         modules = [
           commonModule
-          { config, pkgs, lib, ... }: {
-            # Agent-specific configuration
-            services.k3s = {
-              serverAddr = "https://10.24.0.254:6443"; # Replace with actual server address
-              role = "agent";
-            };
+          (
+            { config, pkgs, ... }:
+            {
+              # Server-specific configuration
+              services.k3s = {
+                extraFlags = [
+                  "--cluster-cidr 10.42.0.0/16"
+                  "--service-cidr 10.43.0.0/16"
+                  "--cluster-dns 10.43.0.10"
+                  "--flannel-backend none" # We'll use Calico for networking
+                ];
+                role = "server";
+                disableAgent = true;
+              };
 
-            # Auto-upgrade settings
-            system.autoUpgrade = {
-              flake = "github:avunu/k3s#server";
-              allowReboot = true;
-            };
-          }
+              # Auto-upgrade settings
+              system.autoUpgrade = {
+                flake = "github:avunu/k3s#server";
+                allowReboot = false;
+              };
+            }
+          )
         ];
       };
     };
-  };
 }
