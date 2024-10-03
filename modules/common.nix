8008414060b32928c1369fc9afe1d4700da553e9
Avunu LLC <mail@avu.nu>
2024-10-03 15:24:19 +0000

    add trusted-users

--- modules/common.nix
+++ modules/common.nix
@@ -82,6 +82,11 @@ in
       trusted-public-keys = [
         "k3s:A8GYNJNy2p/ZMtxVlKuy1nZ8bnZ84PVfqPO6kg6A6qY="
       ];
+      trusted-users = [
+        "root"
+        "@nixbld"
+        "@wheel"
+      ];
     };
   };
 
