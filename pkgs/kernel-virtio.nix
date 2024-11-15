752358060af15d84cc610087394fb1c6f8adc688
Maëlys Bras de fer <mae.bdf@outlook.com>
2024-11-15 20:55:52 +0100

    Build optimized kernels

---
+++ pkgs/kernel-virtio.nix
@@ -0,0 +1,17 @@
+{ lib, ccacheClangStdenv, linux_latest, ... }:
+
+linux_latest.override {
+  stdenv = ccacheClangStdenv;
+
+  extraMakeFlags = [
+    "LLVM=1"
+    "KCFLAGS+=-O3"
+    "KCFLAGS+=-march=haswell"
+  ];
+
+  structuredExtraConfig = with lib.kernel; {
+    LTO_CLANG_FULL = yes;
+  };
+
+  ignoreConfigErrors = true;
+}
